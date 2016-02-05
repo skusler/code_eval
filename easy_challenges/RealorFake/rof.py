514a97d455453a81a368da334a6199bf7d6fb40d
skusler <stephen.kusler@gmail.com>
2016-02-05 10:18:51 -0600

    Update rof.py

--- easy_challenges/RealorFake/rof.py
+++ easy_challenges/RealorFake/rof.py
@@ -4,18 +4,17 @@ import sys
 def rof_test():
     with open(sys.argv[1], 'r') as test_cases:
         for line in test_cases:
-            double = []
-            non_double = []
+            result = []
             count = 0
             while count < 19:
                 if line[count] == ' ':
                     pass
                 elif (count + 1) % 3 == 0 or (count + 1) == 1:
-                    double.append(int(line[count]) * 2)
+                    result.append(int(line[count]) * 2)
                 else:
-                    non_double.append(int(line[count]))
+                    result.append(int(line[count]))
                 count += 1
-            if (sum(double) + sum(non_double)) % 10 == 0:
+            if sum(result) % 10 == 0:
                 print("Real")
             else:
                 print("Fake")
