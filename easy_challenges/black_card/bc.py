ad0d18650547c4a4f473d826a5d2182c1ad50e82
skusler <stephen.kusler@gmail.com>
2016-02-05 16:49:57 -0600

    Create bc.py

---
+++ easy_challenges/black_card/bc.py
@@ -0,0 +1,17 @@
+# This is for challenge Black Card @ https://www.codeeval.com/open_challenges/222/
+
+import sys
+
+
+def bc():
+    with open(sys.argv[1], 'r') as test_cases:
+        for line in test_cases:
+            game_split = line.split(' | ')
+            names = game_split[0]
+            names = names.split()
+            max_turns = int(game_split[1])
+            count = 0
+
+
+
+bc()
