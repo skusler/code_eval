88683f2b233128a42e2f6e071276cec568207d90
skusler <stephen.kusler@gmail.com>
2016-02-05 16:51:20 -0600

    Update bc.py

--- easy_challenges/black_card/bc.py
+++ easy_challenges/black_card/bc.py
@@ -7,10 +7,12 @@ def bc():
     with open(sys.argv[1], 'r') as test_cases:
         for line in test_cases:
             game_split = line.split(' | ')
-            names = game_split[0]
-            names = names.split()
+            names = game_split[0].split()
             max_turns = int(game_split[1])
             count = 0
+            print(names)
+            print(max_turns)
+            print(count)
 
 
 
